5e008316732e7fc4a336ef507efaa206da9655a3
Vitaliy Sulimenko <prevetal@gmail.com>
2025-03-11 16:37:15 +0400

    Delete console.log()

--- src/components/ibc_recovery/ChooseNetwork.vue
+++ src/components/ibc_recovery/ChooseNetwork.vue
@@ -82,12 +82,9 @@
 
             chainIDs.forEach(chainId => {
                 // Get network config
-                console.log(chainId)
                 chainId === 'space-pussy'
                     ? networkConfig = store.networks.ibc_recovery.space_pussy
                     : networkConfig = chains.find(chain => chain.chain_id === chainId)
-
-                console.log(networkConfig)
 
                 // Set data
                 if (!networks.value.find(el => el.chain_id === networkConfig.chain_id)) {
